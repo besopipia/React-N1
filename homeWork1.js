1925c6a822eda617f02be78ed496c97fdfb72459
besopipia <46895640+besopipia@users.noreply.github.com>
2022-08-27 09:48:17 +0400

    Add files via upload

--- homeWork1.js
+++ homeWork1.js
@@ -13,10 +13,29 @@ console.log(sum);
 // 2) შექმენით მასივი,რომელიც შედგება სტრინგებისგან(რომლებიც შეიძლება განმეორდეს). დაწერეთ კოდი,რომელიც დაითვლის რამდენჯერ მეორდება თითოეული სტრინგი მასივში და დააბრუნეთ ობიექტის სახით, მაგ: { a:1 , b: 6, c : 2}. გამოიყენეთ მასივის reduce მეთოდი.
 
 let arr = ['a','b','c','d','d','e','a','b','c','f','g','h','h','h','e','a'];
-let repCount = arr.reduce(function(prev, cur) {
-  prev[cur] = (prev[cur] || 0) + 1;
+let repeatCount = arr.reduce(function(prev, curr) {
+  prev[curr] = (prev[curr] || 0) + 1;
   return prev;
 }, {});
 
-console.log(repCount);
+console.log(repeatCount);
+
+
+// 4) შექმენით ფუნქცია addAsync,რომელიც იღებს ორ პარამეტრს და აბრუნებს პრომისს. თუ ორივე პარამეტრი გადაეცემა(undefined არ არის და ორივე პარამეტრი რიცხვია) პრომისი უნდა დარიზოლვდეს(resolve), წინააღმდეგ შემთხვევაში დარიჯექთდეს(reject). გამოიძახეთ ფუნქცია რამდენიმეჯერ, სხვადასხვა პარამეტრით და კონსოლში გამოიტანეთ შედეგი.
+
+
+const addSync = (num) => {
+  new Promise((resolve, reject) => {
+    if (num < 10) {
+      resolve("Correct");
+    } else {
+      reject("Wrong!!");
+    }
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+addSync(14); 
+
 
